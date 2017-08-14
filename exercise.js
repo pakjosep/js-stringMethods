3c6bf5be7b0f1b060104efee11c9d48cf4a0da63
Joseph Pak <pakjosep@Josephs-MacBook-Air.local>
2017-08-14 13:55:50 -1000

    Q3

--- exercise.js
+++ exercise.js
@@ -32,7 +32,8 @@ console.log(fifthLetter);
 
 var randomFact = "All pandas in the world are on loan from China.";
 
-
+var worldLocator = randomFact.indexOf("world");
+console.log(worldLocator);
 
 /*The slice() method extracts a part of a string and returns the extracted part into a new string. This method takes 2 parameters: the starting index (position) and the ending index (position)*/
 
